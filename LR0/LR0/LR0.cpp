d2b2c42ecc6f963ccf8567768290412ea1c68ed2
Bestyara <spider6@mail.ru>
2021-09-05 15:22:42 +0300

    Update LR0.cpp

--- LR0/LR0/LR0.cpp
+++ LR0/LR0/LR0.cpp
@@ -5,7 +5,7 @@ int main()
 {
     cout << "Hello World!\n";
     char a;
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");// для вывода в консоль русских символов
     cout << "Введите символ: ";
     cin >> a;
 
